4876a84f4535017fd6306b259c84caf4a92b6ef2
jeff griffin <jeffgriffin@jeffs-Mac-mini.local>
2025-02-07 16:29:54 -0700

    added Clerk auth route

---
+++ twiddleapp/app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+import { Inter } from "next/font/google";
+import { Metadata } from "next";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+
+export const metadata: Metadata = {
+    title: "Droogger",
+    description: "A different place"
+}
+
+const inter = Inter({
+    subsets: ["latin"]
+})
+
+export default function RootLayout({ children } :
+    Readonly<{
+        children: React.ReactNode
+    }>
+){
+    return (
+        <>
+            <html lang='en'>
+                <ClerkProvider >
+                    <body>
+                        <main className={`${inter.className} bg-dark-1` }>
+                            <div className="w-full flex justify-center items-center min-h-screen">
+                                { children }
+                            </div>
+                        </main>
+                    </body>
+                </ClerkProvider>
+                
+            </html>
+        </>
+    )
+}
