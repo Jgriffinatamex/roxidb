410bff15cca4cc1490f90a6d06bc7a17edf59b5a
jeff griffin <jeffgriffin@jeffs-Mac-mini.local>
2025-02-20 10:50:32 -0700

    codemod fixes v3

--- twiddleapp/components/shared/NewLeftSidebar.tsx
+++ twiddleapp/components/shared/NewLeftSidebar.tsx
@@ -29,20 +29,21 @@ const LeftSidebar = () => {
 
     ]
     return(
-        <div className='col-span-1 h-full pr-4 md:pr-6'>
-            <div className='flex flex-col items-end'>
-                <div className='space-y-2 lg:w-[230px]'>
-                    {items.map((item) => (
-                        <SidebarItem
-                            key={item.href}
-                            label={item.label}
-                            href={item.href}
-                            icon={item.icon}
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
+        <></>
+        // <div className='col-span-1 h-full pr-4 md:pr-6'>
+        //     <div className='flex flex-col items-end'>
+        //         <div className='space-y-2 lg:w-[230px]'>
+        //             {items.map((item) => (
+        //                 <SidebarItem
+        //                     key={item.href}
+        //                     label=''
+        //                     href={item.href}
+        //                     icon={item.icon}
+        //                 />
+        //             ))}
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 export default LeftSidebar;
