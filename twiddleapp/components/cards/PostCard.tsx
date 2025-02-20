6d27614a05c0bd49c09413e531f6c10947a48d0d
jeff griffin <jeffgriffin@jeffs-Mac-mini.local>
2025-02-20 10:39:19 -0700

    installing codemod

--- twiddleapp/components/cards/PostCard.tsx
+++ twiddleapp/components/cards/PostCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { formatDateString } from '@/lib/utils'; 
 import PostLikeButton from "../shared/PostlikeButton";
-import SharePostButton from "../shared/SharePostButton";
+//import SharePostButton from "../shared/SharePostButton";
 import RepostButton from "../shared/RepostButton";
 import DeletePostButton from "../shared/DeletePostButton";
 
